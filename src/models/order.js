d56a6d09d6747999d5dc1b7bba2d82defa038cbd
Bảo Trương <truonggiabao203@gmail.com>
2024-12-24 20:47:53 +0700

    update date when create and fix api

--- src/models/order.js
+++ src/models/order.js
@@ -1,31 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    // Sender
-    senderName: String,
-    senderNumber: String,
-    fromAddress: String,
-    fromDistrict: String,
-    fromCity: String,
+  // Sender
+  senderName: String,
+  senderNumber: String,
+  fromAddress: String,
+  fromDistrict: String,
+  fromCity: String,
 
-    // Recipient
-    recipientName: String,
-    recipientNumber: String,
-    toAddress: String,
-    toDistrict: String,
-    toCity: String,
+  // Recipient
+  recipientName: String,
+  recipientNumber: String,
+  toAddress: String,
+  toDistrict: String,
+  toCity: String,
 
-    // Order info
-    orderWeight: Number,
-    orderSize: Number,
-    type: String,
-    message: String,
-    price: Number,
-    status: String,
-    createdBy: String,
-    driver: String
+  // Order info
+  orderWeight: Number,
+  orderSize: Number,
+  type: String,
+  message: String,
+  price: Number,
+  status: String,
+  createdBy: String,
+  driver: String,
+  // Creation date
+  createdAt: { type: Date, default: Date.now },
 });
 
-const Order = mongoose.model('order', userSchema);
+const Order = mongoose.model("order", userSchema);
 
 module.exports = Order;
